36b0aad20b00ebae1899362ad3e5a9bd6b4fb552
Salwa Souissi <s.souissi@root.sa>
2018-03-02 12:58:26 +0100

    Add project files.

---
+++ UIDesignTest2/UIDesignTest2/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace UIDesignTest2
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+            //MainPage = new UIDesignTest2.MasterPage2();
+            MainPage = new NavigationPage(new UIDesignTest2.Page1())
+            {
+                BarBackgroundColor = Color.FromHex("#514c50")
+            };
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
